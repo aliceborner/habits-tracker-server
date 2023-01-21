52c5c3cba2aa7ff735f0b7049b75bde8c461e096
Alice Borner <alicezbo@gmail.com>
2023-01-21 20:34:01 +0100

    Starting prisma and fastify

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+import Fastify from "fastify";
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.get("/hello", async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: "Passear",
+      },
+    },
+  });
+  return habits;
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP server running");
+  });
